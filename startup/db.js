0e8ecf18fe07f1fd8982a00ff691fcefb0f21cdd
nevilleadrian <nchukumah@infomall.ng>
2020-06-05 00:01:01 +0100

    fixed bug

--- startup/db.js
+++ startup/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const config = require('config')
 
+const MONGODB_URL = "mongodb+srv://filtereduser:osasere1994$@filtered-redm0.mongodb.net/<dbname>?retryWrites=true&w=majority"
+
 function db() {
     const db = config.get('db');
     mongoose.connect( db,  { useNewUrlParser: true ,useUnifiedTopology: true, useCreateIndex: true})
