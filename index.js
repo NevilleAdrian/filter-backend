b429d90bd2440463fded1fa033bf51de2ea7fd4f
nevilleadrian <nchukumah@infomall.ng>
2020-06-05 19:37:41 +0100

    fixed cors issue-4

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ require('./startup/routes')(app)
 require('./startup/db')();
 require('./startup/prod')(app);
 
-// if (process.env.NODE_ENV === 'Production') {
-    
-// }
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`)); 
--- startup/routes.js
+++ startup/routes.js
@@ -4,24 +4,19 @@ var cors = require('cors')
 const error = require('../middleware/error')
 
 function routes(app)  {
- app.use(function (req, res, next) {
-
-        // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-    
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
-    
-        // Pass to next layer of middleware
-        next();
- });
+app.use(function (req, res, next) {
+       res.setHeader("Access-Control-Allow-Origin", "*");
+       res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
+       res.setHeader("Access-Control-Allow-Credentials", "true");
+       res.setHeader(
+              "Access-Control-Allow-Headers",
+              "x-access-token,X-Requested-With,Content-Type,Authorization,cache-control"
+       );
+       res.setHeader("X-Powered-By", "MadtServerInjunctions!");
+       next();
+});
+       
+ 
 app.use(express.json());
 app.use('/api/filters', filters);
 app.use(cors());
