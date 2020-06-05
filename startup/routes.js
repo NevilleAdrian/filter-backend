505c09f2a543727daf518e0cf25e502f4789cfe7
nevilleadrian <nchukumah@infomall.ng>
2020-06-05 19:45:03 +0100

    fixed cors issue-5

--- startup/routes.js
+++ startup/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const filters = require('../routes/filters');
 var cors = require('cors')
-app.use(cors());
 const error = require('../middleware/error')
 
 function routes(app)  {
 
 app.use(express.json());
+app.use(cors());
 
 app.use(function (req, res, next) {
        res.setHeader("Access-Control-Allow-Origin", "*");
