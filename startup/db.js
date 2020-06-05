2dd5a8eab1cf4b2a983e4f8156c2c7d603b011ac
nevilleadrian <nchukumah@infomall.ng>
2020-06-05 01:47:20 +0100

    ..

--- startup/db.js
+++ startup/db.js
@@ -6,7 +6,7 @@ const config = require('config')
 function db() {
     const db = config.get('db');
     const MONGODB_URL = `mongodb+srv://neville:<osasere1994$>@vidly-loeg5.mongodb.net/${db}?retryWrites=true&w=majority`
-    mongoose.connect( MONGODB_URL,  { useNewUrlParser: true ,useUnifiedTopology: true, useCreateIndex: true})
+    mongoose.connect( process.env.MONGODB_URL,  { useNewUrlParser: true ,useUnifiedTopology: true, useCreateIndex: true})
     .then(() => console.log(`Connected to ${db}`))
     .catch(err => console.error('Could not connect to MongoDB...'));
   
