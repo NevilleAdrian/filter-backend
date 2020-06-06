4a23408ecf19b642ff9a00118c7c181d85bea2ae
nevilleadrian <nchukumah@infomall.ng>
2020-06-06 21:50:23 +0100

    '..'

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ require('./startup/routes')(app)
 require('./startup/db')();
 require('./startup/prod')(app);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`)); 
