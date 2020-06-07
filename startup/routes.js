26e9b75cbd5797e51e85206a358772f573db1757
nevilleadrian <nchukumah@infomall.ng>
2020-06-07 23:06:32 +0100

    cors issue

--- startup/routes.js
+++ startup/routes.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 
 app.use(function(req, res, next) {
-       res.header("Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Origin", "https://filtered-neville.herokuapp.com/");
        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
        next();
 });
