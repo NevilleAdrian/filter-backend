67621fee10e1c65960840ceaf46456266ac5e5a5
nevilleadrian <nchukumah@infomall.ng>
2020-06-08 01:23:48 +0100

    cotrs debugging

--- startup/routes.js
+++ startup/routes.js
@@ -1,20 +1,10 @@
 const express = require('express');
-var cors = require('cors');
 const filters = require('../routes/filters');
 const error = require('../middleware/error')
 
 function routes(app)  {
 
 app.use(express.json());
-
-
-app.use(function(req, res, next) {
-       res.header("Access-Control-Allow-Origin", "https://filtered-neville.herokuapp.com/");
-       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-       next();
-});
-     
-
 app.use('/api/filters', filters);
 app.use(error);
 
