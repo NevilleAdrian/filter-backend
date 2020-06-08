7f33fc8ede330ebbd88a1aacd57d2cbfa299f76c
nevilleadrian <nchukumah@infomall.ng>
2020-06-08 02:31:56 +0100

    Tried adding cors to route page

--- startup/routes.js
+++ startup/routes.js
@@ -1,21 +1,11 @@
 const express = require('express');
-import cors from 'cors';
 const filters = require('../routes/filters');
 const error = require('../middleware/error')
 
 
 function routes(app)  {
 
-app.use(express.json());
-
-app.use(function(req,res,next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'OPTIONS');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-})
-app.use(cors());
-     
+app.use(express.json());  
 app.use('/api/filters', filters);
 app.use(error);
 
