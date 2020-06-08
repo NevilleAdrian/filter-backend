526f86a535617efc3c75bd4601da1a9e0a6db48c
nevilleadrian <nchukumah@infomall.ng>
2020-06-08 02:01:49 +0100

    Tried adding cors to route page

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ const express = require('express');
 var cors = require('cors');
 const app = express();
 
-// Allow all
-app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  //intercepts OPTIONS method
+  if ('OPTIONS' === req.method) {
+    //respond with 200
+    res.send(200);
+  }
+  else {
+  //move on
+    next();
+  }
+});
 
 require('./startup/routes')(app)
 require('./startup/db')();
