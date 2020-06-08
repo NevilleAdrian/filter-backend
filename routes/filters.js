a98da91589f21be18dfe9b254c805a28913f1745
nevilleadrian <nchukumah@infomall.ng>
2020-06-08 01:53:34 +0100

    cors issue

--- routes/filters.js
+++ routes/filters.js
@@ -6,6 +6,12 @@ var cors = require('cors');
 const paginate = require('jw-paginate');
 
 
+router.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.send(200);
+});
 
 router.get('/', cors(), async (req, res) => {
   
